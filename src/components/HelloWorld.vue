6cf0c5f7ce14480e715c39a9afa294ad1d4d6c08
songyubin <1970083855@qq.com>
2018-12-26 18:57:55 +0800

    async

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,17 +3,65 @@
     <router-link to="/home">跳转到home51</router-link>
     <h1 class="rr">{{ msg }}</h1>
     <h2>Essential Links g b v 66r236</h2>
+    <div slot="xx">dsgg</div>
+    <slot style='color:blue;'>这是在slot上添加了样式</slot>
+    <slot name="xx">
+      <div>我是个插槽</div></slot>
+    <div>{{bb}}</div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      bb:''
     }
-  }
+  },
+      mounted(){
+
+        function sleep(second, param) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              console.log('---1')
+              resolve(param);
+
+            }, second);
+          })
+        }
+        function aaa(i){
+          return 	axios.get('http://jx.xuzhixiang.top/ap/api/productlist.php',{})
+            .then(function(data){
+
+              console.log(i,'---aaa')
+              return 5555
+            })
+        }
+        function ccc(e){
+          axios.get('http://jx.xuzhixiang.top/ap/api/productlist.php',{})
+            .then(function(data){
+              console.log(e,'---bbb')
+            })
+        }
+        async function test() {
+          let result1 = await sleep(2000, 'req01');
+          let result2 = await aaa(result1);
+          let result3 = await ccc(result2);
+        }
+
+        test();
+
+
+
+
+
+
+
+    }
+
 }
 </script>
 
